c4af0ef28a0c98e2d6d452620de7e8d28d086de6
Manases <2791808530307@ingenieria.usac.edu.gt>
2021-10-02 08:44:53 -0600

    Endpoints para reportes.

--- backend/src/models/querys.js
+++ backend/src/models/querys.js
@@ -14,6 +14,59 @@ class Querys{
         contrasenia = '${persona.contrasenia}'
         WHERE usuarios_id = ${persona.usuarios_id};`;    
     }
+
+    actualizar_logro(logro){
+        return `UPDATE logro_academico 
+        SET anio = ${logro.anio}, titulo = '${logro.titulo}', tipo_logro_tipo_logro_id = ${logro.tipo_id}, 
+        institucion_institucion_id = ${logro.institucion}
+        WHERE logro_id = ${logro.logro_id};`
+    }
+
+    actulizar_area(area, persona){
+        return `UPDATE area_desempenio 
+        SET areas_estudio_area_estudio_id = ${area}
+        WHERE persona_usuarios_id = ${persona};`;
+    }
+
+    agregar_desempeño(area, persona){
+        return `INSERT INTO area_desempenio (areas_estudio_area_estudio_id, persona_usuarios_id)
+        VALUES (${area}, ${persona});`;        
+    }
+
+    agregar_logro(logro){
+        return `INSERT INTO logro_academico (anio, titulo, tipo_logro_tipo_logro_id, persona_usuarios_id, institucion_institucion_id)
+        values (${logro.anio}, '${logro.titulo}', ${logro.tipo_logro}, ${logro.persona}, ${logro.institucion});`;
+    }
+
+    quitar_desempeño(area, persona){
+        return `DELETE FROM area_desempenio a
+        WHERE a.areas_estudio_area_estudio_id = ${area} and a.persona_usuarios_id = ${persona};`;
+    }
+
+    quitar_logro(logro_id){
+        return `DELETE FROM logro_academico l
+        WHERE l.logro_id = ${logro_id};`;
+    }
+
+    reporte_personas(){
+        return `SELECT usuarios_id, CONCAT_WS(' ', p2.primer_nombre, p2.primer_apellido) as nombre,
+        p2.genero,
+        (SELECT CONCAT_WS(' ', la.anio, i.nombre, tl.descripcion, la.titulo) FROM persona p 
+        INNER JOIN logro_academico la ON p.usuarios_id = la.persona_usuarios_id 
+        INNER JOIN tipo_logro tl ON tl.tipo_logro_id = la.tipo_logro_tipo_logro_id 
+        INNER JOIN institucion i ON i.institucion_id = la.institucion_institucion_id 
+        WHERE p.usuarios_id = p2.usuarios_id 
+        ORDER BY la.anio DESC 
+        LIMIT 1) as Titulo
+        FROM persona p2 ;`;
+    }
+
+    reporte_areas(){
+        return `SELECT ae.nombre, COUNT(*) as cantidad FROM areas_estudio ae 
+        INNER JOIN area_desempenio ad ON ae.area_estudio_id = ad.areas_estudio_area_estudio_id 
+        INNER JOIN persona p ON p.usuarios_id = ad.persona_usuarios_id 
+        GROUP BY ae.nombre ;`;
+    }
 }
 
 module.exports = {Querys};
--- backend/src/routes/routes.js
+++ backend/src/routes/routes.js
@@ -6,10 +6,20 @@ const app = Router();
 const user = require('../controllers/users');
 
 app.post('/login', user.Login);
-app.put('/user/update', user.Actualizar);
+app.put('/upadate/user', user.Actualizar);
+app.put('/update/logro', user.ActualizarLogro);
+app.put('/update/area', user.ActualizarArea);
+app.post('/add-area-desempenio', user.AgregarDesempeño);
+app.post('/add-logro', user.AgregarLogro);
+app.delete('/delete/desempenio/:area/:persona', user.QuitarDesempeño);
+app.delete('/delete/logro/:logro_id', user.QuitarLogro);
 
 
+//Rutas para obtener los reportes
+const reporte = require('../controllers/reportes');
 
+app.get('/reporte/personas', reporte.Personas);
+app.get('/reporte/areas-desempenio', reporte.AreasDesempeño);
 
 
 
